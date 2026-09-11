6bfe612c775514c0c0aaef46c21770bf5143df36
habyarimanacassien <chabyarimana@aimsric.org>
2026-09-12 00:09:17 +0200

    connect with mongodb and add a get and getAll

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+mongodb.initdb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node isrunning on ${port}`);
+        });
+    }
 });
