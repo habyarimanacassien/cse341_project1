4ffc74bfb6bae07e7aa31c5446bc6478712c6bcc
habyarimanacassien <chabyarimana@aimsric.org>
2026-09-26 13:36:26 +0200

    improve swagger

--- swagger.js
+++ swagger.js
@@ -1,23 +1,16 @@
-const fs = require('fs');
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
     info: {
         title: 'Users Api',
         description: 'Users Api'
-    }
+    },
+    host: '',
+    schemes: []
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-// Generate swagger.json, then remove "host" and "schemes" (swagger-autogen
-// adds localhost:3000/http by default). Without them, Swagger UI sends every
-// request to the same server that serves /api-docs, so the same file works
-// on localhost AND on Render.
-swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-    const spec = JSON.parse(fs.readFileSync(outputFile, 'utf8'));
-    delete spec.host;
-    delete spec.schemes;
-    fs.writeFileSync(outputFile, JSON.stringify(spec, null, 2));
-});
+// this will generate the swagger.json file
+swaggerAutogen(outputFile, endpointsFiles, doc);
